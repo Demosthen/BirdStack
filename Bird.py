50351b6497f8dcff74f2e6c2db818c15c21b9545
jzhou1318 <jennifersiyuanzhou@berkeley.edu>
2019-11-22 23:29:44 -0800

    Added Bird.py and Load.py

---
+++ Bird.py
@@ -0,0 +1,38 @@
+gravity = 9
+class Bird(pygame.sprite.Sprite):
+    move = 9
+    bird_size = (50,50)
+    def __init__(self, startPos = (100,100)):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image('scooter.png', -1, startPos)
+        screen = pygame.display.get_surface()
+        self.dropping = False
+        self.stationary = False
+        self.rect.center = startPos
+        self.area = screen.get_rect()
+
+    def update(self):
+        if not self.stationary:
+            if self.dropping:
+                self.drop()
+            else:
+                self.fly()
+
+    def fly(self):
+        """move the bird across the screen, and turn at the ends"""
+        newpos = self.rect.move((self.move, 0))
+        if not self.area.contains(newpos):
+            if self.rect.left < self.area.left or \
+                    self.rect.right > self.area.right:
+                self.move = -self.move
+                newpos = self.rect.move((self.move, 0))
+                self.image = pygame.transform.flip(self.image, 1, 0)
+        self.rect = newpos
+
+    def drop(self):
+        """make the bird fall"""
+        newpos = self.rect.move((0, gravity))
+        self.rect = newpos
+        if self.rect.top >= self.area.height:
+            print("DEAD!")
+            self.kill()
--- Main.py
+++ Main.py
@@ -1,77 +1,11 @@
 import os, sys
 import pygame
 from pygame.locals import *
+import Bird
+import Load
 
 if not pygame.font: print('Warning, fonts disabled')
 if not pygame.mixer: print('Warning, sound disabled')
-
-gravity = 9
-
-def load_image(name, colorkey=None, sizes = (50,50)):
-    fullname = os.path.join('data', name)
-    try:
-        image = pygame.image.load(fullname)
-        image = pygame.transform.scale(image, sizes)
-    except pygame.error as message:
-        print('Cannot load image:', name)
-        raise SystemExit(message)
-    image = image.convert()
-    if colorkey is not None:
-        if colorkey is -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey, RLEACCEL)
-    return image, image.get_rect()
-def load_sound(name):
-    class NoneSound:
-        def play(self): pass
-    if not pygame.mixer:
-        return NoneSound()
-    fullname = os.path.join('data', name)
-    try:
-        sound = pygame.mixer.Sound(fullname)
-    except pygame.error as message:
-        print('Cannot load sound:', wav)
-        raise SystemExit(message)
-    return sound
-
-class Bird(pygame.sprite.Sprite):
-    move = 9
-    bird_size = (50,50)
-    def __init__(self, startPos = (100,100)):
-        pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_image('scooter.png', -1, startPos)
-        screen = pygame.display.get_surface()
-        self.dropping = False
-        self.stationary = False
-        self.rect.center = startPos
-        self.area = screen.get_rect()
-
-    def update(self):
-        if not self.stationary:
-            if self.dropping:
-                self.drop()
-            else:
-                self.fly()
-
-    def fly(self):
-        """move the bird across the screen, and turn at the ends"""
-        newpos = self.rect.move((self.move, 0))
-        if not self.area.contains(newpos):
-            if self.rect.left < self.area.left or \
-                    self.rect.right > self.area.right:
-                self.move = -self.move
-                newpos = self.rect.move((self.move, 0))
-                self.image = pygame.transform.flip(self.image, 1, 0)
-        self.rect = newpos
-
-    def drop(self):
-        """make the bird fall"""
-        newpos = self.rect.move((0, gravity))
-        self.rect = newpos
-        if self.rect.top >= self.area.height:
-            print("DEAD!")
-            self.kill()
-
 
 def run():
     pygame.init()
